5073a5dc525e25741c0af619d3c40448571038e3
Will Wilcox <wwilcox88@gmail.com>
2017-01-26 12:27:11 -0500

    added more stuff to mock form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import "rxjs/add/observable/combineLatest";
+import "rxjs/add/operator/filter";
 
 @Component({
   selector: 'app-root',
   template: `
   <form #formRef="ngForm" (ngSubmit)="onSubmit(formRef.value)">
-    <fieldset ngModelGroup="login">
+    <fieldset ngModelGroup="new-test-user">
       <input
         required
         type="text"
@@ -19,7 +23,26 @@ import { Component } from '@angular/core';
 
 
       <div>{{usernameRef.errors | json}}</div>
-      <button type="submit">Submit</button>
+      <button type="submit">Submit</button> br
+      <label>Current City</label>
+      <div *ngFor="let location of locations">
+        <input type="radio"
+          name="location"
+          ngModel
+          [attr.id]="location"
+          [value]="location"
+        >
+        <label [attr.for]="location">{{location}}</label>
+      </div>
+      <div>
+        <label>Favorite Food</label>
+        <select name="food" ngModel>
+          <option>--Select--</option>
+          <option *ngFor="let food of foods"
+            [value]="food"
+          >{{food}}</option>
+        </select>
+      </div>
     </fieldset>
   </form>
 
@@ -35,12 +58,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   username = 'Will';
+  locations = ['nyc', 'la', 'nola'];
+  foods = ['hotdog', ' pizza', 'broccoli'];
+
+  @ViewChild('formRef') form;
 
   onSubmit(data){
     console.log(data);
-
   }
 
+  ngAfterViewInit(){
+    this.form.statusChanges
+      .subscribe(w => console.log(w));
 
+    Observable.combineLatest(
+      this.form.statusChanges,
+      this.form.valueChanges,
+      (status, value) => ({status, value})
+    )
+      .filter(({status})=> status ==='VALID')
+      .subscribe(({value})=> console.table(value))
+  }
 
 }
